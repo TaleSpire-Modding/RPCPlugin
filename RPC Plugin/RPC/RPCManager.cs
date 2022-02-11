3fb741ac05951e263b6e7fbb3b9c88f10cd4d9cf
brajm008 <ujkkju117@outlook.com>
2022-02-11 21:08:12 +1030

    Add project files.

---
+++ RPC Plugin/RPC/RPCManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Bounce.Unmanaged;
+using Newtonsoft.Json;
+using Photon;
+using UnityEngine;
+
+namespace RPCPlugin.RPC
+{
+    public class RPCManager : PunBehaviour
+    {
+        public enum ChatSource
+        {
+            gm = 0,
+            player = 1,
+            creature = 2,
+            anonymous = 999
+        }
+
+        public static Dictionary<string, Func<string, string, ChatSource, string>> handlers = new Dictionary<string, Func<string, string, ChatSource, string>>();
+
+        public static RPCManager Instance;
+        public static PhotonView View;
+
+        public static void Init()
+        {
+            if (Instance == null)
+            {
+                var o = new GameObject();
+                var rpc = Instantiate(o);
+                Instance = rpc.AddComponent<RPCManager>();
+                View = rpc.AddComponent<PhotonView>();
+                View.viewID = 606;
+            }
+        }
+
+        public static void SendChatMessage(string message, NGuid thingThatIsTalking)
+        {
+            PhotonView photonView = View;
+
+            ChatSource source = ChatSource.anonymous;
+            if (LocalPlayer.Id.Value == thingThatIsTalking)
+                source = CampaignSessionManager.PlayersInfo[LocalPlayer.Id].Rights.CanGm ?  
+                    ChatSource.gm : ChatSource.player;
+            foreach (CreatureBoardAsset asset in CreaturePresenter.AllCreatureAssets)
+            {
+                if (asset.Creature.UniqueId.Value == thingThatIsTalking)
+                {
+                    source = ChatSource.creature;
+                }
+            }
+            photonView.RPC(nameof(ReceivedMessage), PhotonTargets.All, new object[] { message, thingThatIsTalking.ToString(), JsonConvert.SerializeObject(source) });
+        }
+
+        [PunRPC]
+        public void ReceivedMessage(string message, string thingThatIsTalking, string chatsource)
+        {
+            Debug.Log($"({message},{thingThatIsTalking},{chatsource})");
+            var source = JsonConvert.DeserializeObject<ChatSource>(chatsource);
+
+            foreach (KeyValuePair<string, Func<string, string, ChatSource, string>> handler in handlers)
+            {
+                if (message.StartsWith(handler.Key))
+                {
+                    _ = handler.Value(message,thingThatIsTalking,source);
+                }
+            }
+        }
+    }
+}
---
+++ RPC Plugin/RPCPlugin.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using RPCPlugin.RPC;
+using UnityEngine;
+
+namespace RPCPlugin
+{
+    [BepInPlugin(Guid, Name, Version)]
+    public class RPCPlugin: BaseUnityPlugin
+    {
+        // Plugin info
+        public const string Name = "HolloFoxes' RPC Plug-In";
+        public const string Guid = "org.hollofox.plugins.rpc";
+        public const string Version = "1.0.0.0";
+        public static RPCPlugin _instance;
+        internal static ManualLogSource BPLogger;
+
+        /// <summary>
+        /// Method triggered when the plugin loads
+        /// </summary>
+        public void Awake()
+        {
+            BPLogger = Logger;
+            Logger.LogInfo($"In Awake for {Name}");
+            _instance = this;
+            // RPCManager.handlers.Add("/w",handler);
+        }
+
+        /*private string handler(string arg1, string arg2, RPCManager.ChatSource arg3)
+        {
+            Debug.Log($"This is the handler:{arg1},{arg2},{arg3}");
+            return arg1;
+        }*/
+
+        /// <summary>
+        /// Triggered every frame
+        /// </summary>
+        public void Update()
+        {
+            if (PhotonNetwork.inRoom && RPCManager.Instance == null)
+            {
+                RPCManager.Init();
+            }
+            if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyUp(KeyCode.S))
+            {
+                RPCManager.SendChatMessage("/w This is a test", LocalPlayer.Id.Value);
+            }
+        }
+    }
+}
